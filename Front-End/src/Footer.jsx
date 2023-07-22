4b3e6591988bae6e6778bec1e5192d251708846c
Chandula Adhikari <e19008@eng.pdn.ac.lk>
2023-07-22 17:12:53 +0530

    Update Footer.jsx

--- Front-End/src/Footer.jsx
+++ Front-End/src/Footer.jsx
@@ -1,14 +1,28 @@
 import "./Footer.css";
+import { useNavigate, useLocation, Form } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 
 export const Footer = () => {
+  const navigate = useNavigate();
+  const [role, setRole] = useState("");
   return (
     <footer className="bottom">
       <div className="legal">
         <div className="words">
           <span> © 2023 All rights reserved </span>
-          <a> License </a>
+          {/* <a> License </a>
           <a> Terms </a>
-          <a> Privacy </a>
+          <a>  </a> */}
+          <form className="flex gap-5">{/* <input
+                      type="text"
+                      id="Parent"
+                      name="role"
+                      value={role}
+                      onChange={(e) => setRole(e.target.value)}
+                    /> */}
+          <a><button onClick={navigate("/parent")}>License</button></a>
+          <a><button onClick={navigate("/midwife")}>Terms</button></a>
+          <a><button onClick={navigate("/doctor")}>Privacy</button></a></form>
         </div>
       </div>
     </footer>
